bbeb141630169ffb9d343dd9c34b0126c49a4183
Kyle Jorve <kyle.jorve@gmail.com>
2022-06-17 19:01:05 -0700

    initial react build

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,10 @@
+import { Route, Routes, Naviate } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment } from "react";
+
+function App() {
+  return <Fragment></Fragment>;
+}
+
+export default App;
